343d2df139c0e3ecf5cd4896270339a400a53742
Ismail Bentabet <ismailbentabett@gmail.com>
2023-03-30 20:24:31 +0100

    yaaay

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import HomeTwo from "./Pages/Home/HomeTwo";
 import PortfiloTwo from "./Pages/Portfilo/PortfiloTwo";
 import ResumeTwo from "./Pages/Resume/ResumeTwo";
 import NotFound from "./Share/NotFound";
+import { Helmet } from "react-helmet";
 
 function App() {
   useEffect(() => {
@@ -19,10 +20,50 @@ function App() {
   }, []);
   return (
     <>
+      <Helmet>
+        <title>ismailbentabett</title>
+        <meta
+          name="description"
+          content="A Multi Talented , multi disciplinary Freelance Full-Stack Software developer and UI/UX Designer , Computer Science Student , Business enthusiast"
+        />
+
+        <meta itemprop="name" content="ismailbentabett" />
+        <meta
+          itemprop="description"
+          content="A Multi Talented , multi disciplinary Freelance Full-Stack Software developer and UI/UX Designer , Computer Science Student , Business enthusiast"
+        />
+        <meta
+          itemprop="image"
+          content="https://scontent.ftlm1-2.fna.fbcdn.net/v/t39.30808-6/326070679_5776791742438597_7519612538097957996_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ZaoLmy8iffMAX9yib3y&_nc_ht=scontent.ftlm1-2.fna&oh=00_AfC2Qu3P39Gdeb7yxyndjOILb6oBAQU5UjY9o0DmCpNJHw&oe=642A3A70"
+        />
+
+        <meta property="og:url" content="https://ismailbentabett.github.io" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="ismailbentabett" />
+        <meta
+          property="og:description"
+          content="A Multi Talented , multi disciplinary Freelance Full-Stack Software developer and UI/UX Designer , Computer Science Student , Business enthusiast"
+        />
+        <meta
+          property="og:image"
+          content="https://scontent.ftlm1-2.fna.fbcdn.net/v/t39.30808-6/326070679_5776791742438597_7519612538097957996_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ZaoLmy8iffMAX9yib3y&_nc_ht=scontent.ftlm1-2.fna&oh=00_AfC2Qu3P39Gdeb7yxyndjOILb6oBAQU5UjY9o0DmCpNJHw&oe=642A3A70"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="ismailbentabett" />
+        <meta
+          name="twitter:description"
+          content="A Multi Talented , multi disciplinary Freelance Full-Stack Software developer and UI/UX Designer , Computer Science Student , Business enthusiast"
+        />
+        <meta
+          name="twitter:image"
+          content="https://scontent.ftlm1-2.fna.fbcdn.net/v/t39.30808-6/326070679_5776791742438597_7519612538097957996_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ZaoLmy8iffMAX9yib3y&_nc_ht=scontent.ftlm1-2.fna&oh=00_AfC2Qu3P39Gdeb7yxyndjOILb6oBAQU5UjY9o0DmCpNJHw&oe=642A3A70"
+        />
+        
+      </Helmet>
       <ContextProvider>
         <HashRouter>
           <Routes>
-
             <Route path="/" element={<HomeTwo />}>
               <Route index element={<AboutTwo />} />
               <Route path="about" element={<AboutTwo />} />
