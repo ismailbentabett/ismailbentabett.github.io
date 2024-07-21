27e6ed7dd98026b198facbbb306c1c120c5992a2
Ismail Bentabet <ismailbentabett@gmail.com>
2024-07-21 17:52:42 +0100

    Deploy to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
-	css: [
-		// "~/assets/css/aos.css",
-		// "~/assets/css/bootstrap.min.css",
-		"~/assets/css/iconoir.css",
-		'bootstrap/dist/css/bootstrap.css',
-		"~/assets/css/style.css",
-	],
+  css: [
+    // "~/assets/css/aos.css",
+    // "~/assets/css/bootstrap.min.css",
+    "~/assets/css/iconoir.css",
+    "bootstrap/dist/css/bootstrap.css",
+    "~/assets/css/style.css",
+  ],
+  plugins: [{ src: "@/plugins/aos", ssr: false }],
 
-	plugins: [{ src: "@/plugins/aos", ssr: false }],
-	
+  app: {
+    baseURL: "/ismailbentabett.github.io/", // Use your repository name
+    buildAssetsDir: "/ismailbentabett.github.io/_nuxt/",
+  },
 });
