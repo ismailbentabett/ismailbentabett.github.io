2b2eaca8e2414fc276b58f588c753393d246a36d
Ismail Bentabet <ismailbentabett@gmail.com>
2024-07-27 15:07:40 +0100

    Deploy Nuxt site to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 export default defineNuxtConfig({
-	css: [
-	  "~/assets/css/iconoir.css",
-	  'bootstrap/dist/css/bootstrap.css',
-	  "~/assets/css/style.css",
-	],
-	plugins: [{ src: "@/plugins/aos", ssr: false }],
-  
-	app: {
-	  baseURL: '/ismailbentabett.github.io/',  // Use your repository name
-	  buildAssetsDir: '/ismailbentabett.github.io/_nuxt/',
-	}
-  });
-  
+  css: [
+    "~/assets/css/iconoir.css",
+    "bootstrap/dist/css/bootstrap.css",
+    "~/assets/css/style.css",
+  ],
+  plugins: [{ src: "@/plugins/aos", mode: "client" }],
+
+  app: {
+    baseURL: "/ismailbentabett.github.io/", // Use your repository name
+  },
+
+  // Add this for GitHub Pages deployment
+  ssr: false,
+  target: "static",
+
+  // Use this instead of buildAssetsDir
+  vite: {
+    base: "/ismailbentabett.github.io/",
+  },
+});
