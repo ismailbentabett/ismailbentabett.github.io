4f7292e6a37c00bd77a922efa6df34f64e9e94b8
Ismail Bentabet <ismailbentabett@gmail.com>
2024-09-05 19:20:16 +0100

    chore: Update Works.vue to display new project images and information

--- components/works/Works.vue
+++ components/works/Works.vue
@@ -1,12 +1,11 @@
-
-
 <template>
   <!-- Projects -->
   <section class="projects-area">
     <div class="container">
       <h1 class="section-heading" data-aos="fade-up">
         <img src="~/assets/images/star-2.png" alt="Star"> All Projects 
-        <img src="~/assets/images/star-2.png" alt="Star"></h1>
+        <img src="~/assets/images/star-2.png" alt="Star">
+      </h1>
       <div class="row">
         <div class="col-md-4">
           <div data-aos="zoom-in">
@@ -14,12 +13,14 @@
               <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
               <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
               <div class="project-img">
-                <img src="~/assets/images/project1.jpeg" alt="Project">
+                <img src="~/assets/images/mm_shop.jpg" alt="Monday Merch - Shop">
               </div>
               <div class="d-flex align-items-center justify-content-between">
                 <div class="project-info">
-                  <p>WEB DESIGNING</p>
-                  <h1>Dynamic</h1>
+                  <p>E-commerce Platform</p>
+                  <h1>Monday Merch - Shop</h1>
+                  <p>Role: Vue Front-end Engineer</p>
+                  <p>Skills: Nuxt.js, Vue.js, Tailwind CSS, TypeScript</p>
                 </div>
                 <NuxtLink to="/work-details" class="project-btn">
                   <img src="~/assets/images/icon.svg" alt="Button">
@@ -33,12 +34,14 @@
               <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
               <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
               <div class="project-img">
-                <img src="~/assets/images/project2.jpeg" alt="Project">
+                <img src="~/assets/images/acile.jpg" alt="Acile App">
               </div>
               <div class="d-flex align-items-center justify-content-between">
                 <div class="project-info">
-                  <p>PHOTOGRAPHY</p>
-                  <h1>Diesel H1</h1>
+                  <p>Mobile Application Development</p>
+                  <h1>Acile App</h1>
+                  <p>Role: Front End Engineer</p>
+                  <p>Skills: Vue.js, Android, iOS, Vuetify</p>
                 </div>
                 <NuxtLink to="/work-details" class="project-btn">
                   <img src="~/assets/images/icon.svg" alt="Button">
@@ -46,24 +49,22 @@
               </div>
             </div>
           </div>
-
         </div>
         <div class="col-md-8">
-          <h1 class="section-heading" data-aos="fade-up"><img src="~/assets/images/star-2.png" alt="Star"> All Projects
-            <img src="~/assets/images/star-2.png" alt="Star"></h1>
-
           <div class="d-flex align-items-start gap-24">
             <div data-aos="zoom-in" class="flex-1">
               <div class="project-item shadow-box">
                 <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
                 <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
                 <div class="project-img">
-                  <img src="~/assets/images/project3.jpeg" alt="Project">
+                  <img src="~/assets/images/mm_platform.jpg" alt="Full-Service Merch Platform">
                 </div>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="project-info">
-                    <p>mOBILE DESIGNING</p>
-                    <h1>Seven Studio</h1>
+                    <p>E-commerce Platform</p>
+                    <h1>The Full-Service Merch Platform | Monday Merch</h1>
+                    <p>Role: Vue Front-end Engineer</p>
+                    <p>Skills: Vue.js, Tailwind CSS, TypeScript</p>
                   </div>
                   <NuxtLink to="/work-details" class="project-btn">
                     <img src="~/assets/images/icon.svg" alt="Button">
@@ -77,12 +78,14 @@
                 <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
                 <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
                 <div class="project-img">
-                  <img src="~/assets/images/project4.jpeg" alt="Project">
+                  <img src="~/assets/images/sig-service.jpg" alt="SIG Service">
                 </div>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="project-info">
-                    <p>Branding</p>
-                    <h1>Raven Studio</h1>
+                    <p>ERP Solutions</p>
+                    <h1>SIG Service - Solutions de gestion informatiques</h1>
+                    <p>Role: Software Engineer</p>
+                    <p>Skills: Vue.js, Vuetify, Tailwind CSS, Android, iOS</p>
                   </div>
                   <NuxtLink to="/work-details" class="project-btn">
                     <img src="~/assets/images/icon.svg" alt="Button">
@@ -98,12 +101,14 @@
                 <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
                 <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
                 <div class="project-img">
-                  <img src="~/assets/images/project5.jpeg" alt="Project">
+                  <img src="~/assets/images/trp.jpg" alt="Telecom Radio">
                 </div>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="project-info">
-                    <p>mOBILE DESIGNING</p>
-                    <h1>Submarine</h1>
+                    <p>Telecommunications Management</p>
+                    <h1>Telecom Radio Professionnel | Motorola Solutions Platform</h1>
+                    <p>Role: Full Stack Developer</p>
+                    <p>Skills: Laravel, Vue.js, Docker, MySQL</p>
                   </div>
                   <NuxtLink to="/work-details" class="project-btn">
                     <img src="~/assets/images/icon.svg" alt="Button">
@@ -117,28 +122,89 @@
                 <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
                 <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
                 <div class="project-img">
-                  <img src="~/assets/images/project6.jpeg" alt="Project">
+                  <img src="~/assets/images/shortcut.jpg" alt="Shortcut">
                 </div>
                 <div class="d-flex align-items-center justify-content-between">
                   <div class="project-info">
-                    <p>wEB DESIGNING</p>
-                    <h1>Hydra Merc</h1>
+                    <p>Startup Platform Development</p>
+                    <h1>Shortcut</h1>
+                    <p>Role: Co-founder and Full Stack Engineer</p>
+                    <p>Skills: Vue.js, Leadership, Entrepreneurship, NestJS</p>
                   </div>
                   <NuxtLink to="/work-details" class="project-btn">
                     <img src="~/assets/images/icon.svg" alt="Button">
                   </NuxtLink>
                 </div>
               </div>
-
             </div>
           </div>
 
+          <div class="d-flex align-items-start gap-24">
+            <div data-aos="zoom-in" class="flex-1">
+              <div class="project-item shadow-box">
+                <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
+                <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
+                <div class="project-img">
+                  <img src="~/assets/images/silikan.png" alt="Silikan">
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="project-info">
+                    <p>Marketplace Development</p>
+                    <h1>Silikan</h1>
+                    <p>Role: Developer</p>
+                    <p>Skills: Laravel, PHP, MySQL, Node.js, JavaScript, Vue.js, Vuex, Tailwind CSS, Socket.io, Redis</p>
+                  </div>
+                  <NuxtLink to="/work-details" class="project-btn">
+                    <img src="~/assets/images/icon.svg" alt="Button">
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+
+            <div data-aos="zoom-in" class="flex-1">
+              <div class="project-item shadow-box">
+                <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
+                <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
+                <div class="project-img">
+                  <img src="~/assets/images/sweetr.jpg" alt="Sweetr">
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="project-info">
+                    <p>Social Media Platform</p>
+                    <h1>Sweetr</h1>
+                    <p>Role: Full Stack Developer</p>
+                    <p>Skills: JavaScript, Firebase</p>
+                  </div>
+                  <NuxtLink to="/work-details" class="project-btn">
+                    <img src="~/assets/images/icon.svg" alt="Button">
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+
+            <div data-aos="zoom-in" class="flex-1">
+              <div class="project-item shadow-box">
+                <NuxtLink class="overlay-link" to="/work-details"></NuxtLink>
+                <img src="~/assets/images/bg1.png" alt="BG" class="bg-img">
+                <div class="project-img">
+                  <img src="~/assets/images/screamer.png" alt="Screamer">
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="project-info">
+                    <p>Social Media Platform</p>
+                    <h1>Screamer</h1>
+                    <p>Role: Developer</p>
+                    <p>Skills: API, JavaScript, Angular, TypeScript, SignalR, .NET Core, Tailwind CSS, C#</p>
+                  </div>
+                  <NuxtLink to="/work-details" class="project-btn">
+                    <img src="~/assets/images/icon.svg" alt="Button">
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   </section>
 </template>
-
- 
-
